e628a8422f0556cb25c1b45b6aca5e849cdffce3
Adam Saunders <saunders@cs.umass.edu>
2014-02-26 19:27:08 -0500

    ssocial ver

--- project/Build.scala
+++ project/Build.scala
@@ -18,29 +18,29 @@ object IeslFrameBuild extends Build {
   import allDeps._
   
   val deps = Seq(
-    "edu.umass.cs.iesl" %% "play-navigator" % "0.4.0" exclude("org.scala-stm", "scala-stm_2.10.0"),
-    "org.apache.commons" % "commons-email" % "1.2", 
-    "org.mindrot" % "jbcrypt" % "0.3m", 
-    "org.ccil.cowan.tagsoup" % "tagsoup" % "latest.release", 
+    "eu.teamon" %% "play-navigator" % "0.5.0",
+    "org.apache.commons" % "commons-email" % "1.2",
+    "org.mindrot" % "jbcrypt" % "0.3m",
+    "org.ccil.cowan.tagsoup" % "tagsoup" % "latest.release",
     
     // it's OK for the webapp to depend on the mongo layer for now, even though it should be entirely independent
     // until the runtime wiring / cake baking
-    "ws.securesocial" %% "securesocial" % "2.1.3", 
-    // "securesocial" %% "securesocial" % "2.1.1" exclude("org.scala-stm", "scala-stm_2.10.0"),
+    "ws.securesocial" %% "securesocial" % "2.1.3",
 
     // todo : figure out which of these are not actually used locally
     // (I had to include them explicitly due to mysterious issues with transitive resolution)
     "net.openreview" %% "scalate-core" % "latest.integration",
     "net.openreview" %% "scalate-core-plus" % "latest.integration",
     "net.openreview" %% "scalate-util" % "latest.integration",
-    //"javax.servlet" % "servlet-api" % "latest.release",
-    scalaCompiler() , 
+
+    scalaCompiler() ,
     scalaIoFile() ,
     scalazCore() ,
-    scalatime() , 
-    ieslScalaCommons("latest.integration") , 
-    "net.sf.ehcache" % "ehcache-core" % "latest.release" , 
-    "org.json4s" %% "json4s-native" % "latest.release" 
+    scalatime() ,
+    ieslScalaCommons("latest.integration") ,
+
+    "net.sf.ehcache" % "ehcache-core" % "latest.release" ,
+    "org.json4s" %% "json4s-native" % "latest.release"
   )
 
   lazy val ieslFrame = {
